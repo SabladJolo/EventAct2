4a2957978c13a12529d801b10f50a4f6b8bb84fd
Jhay1413 <90449169+SabladJolo@users.noreply.github.com>
2021-12-14 15:32:33 +0800

    Committed act2

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'act3'`, () => {
+  it(`should have as title 'act2'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('act3');
+    expect(app.title).toEqual('act2');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('act3 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('act2 app is running!');
   });
 });
